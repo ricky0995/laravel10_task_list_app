5f6b57d76f66455f60f91077c5a3c70d020b8646
ricky0995 <wijayaricky1295@gmail.com>
2023-07-26 15:57:38 +0700

    define a home route and a route for displaying 1 task

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,22 @@
-<div>hello i'm a blade template</div>
+<h1>The List of Tasks</h1>
 
+<div>
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+    {{-- <div>{{ $task->title }}</div> --}}
+  @empty
+    <div>There are no tasks!</div>
+  @endforelse
+</div>
 
-@isset($name)
-  <div>The name is : {{ $name }}</div>
-@endisset
+{{-- <div>
+  @if(count($tasks))
+    @foreach($tasks as $task)
+      <div>{{ $task->title }}</div>
+    @endforeach
+  @else
+    <div>There are no tasks!</div>
+  @endif
+</div> --}}
--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,94 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index', [
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {
+  }
+}
 
+$tasks = [
+  new Task(
+    1,
+    'Buy groceries',
+    'Task 1 description',
+    'Task 1 long description',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Learn programming',
+    'Task 3 description',
+    'Task 3 long description',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    'Take dogs for a walk',
+    'Task 4 description',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
+
+// To Use a Variable with anonymous function, need to type 'use' keyword
+Route::get('/', function () use ($tasks) {
+    return view('index', [
+        'tasks' => $tasks
+        
         // the value will be escaped by laravel, so html/script tag will be displayed as string
         // 
-        'name' => 'Ricky',
+        // 'name' => 'Ricky',
     ]);
     // return view('welcome');
-});
+})->name('tasks.index');
 
-// example of named route
-Route::get('/hello', function(){
-    return 'Hello';
-})->name('hello');
+Route::get('/{id}', function ($id){
+    return 'One single task';
+})->name('tasks.show');
 
-// example tp redirect a route to another route
-Route::get('/hallo', function(){
-    // redirect with normal route url
-    // return redirect('/hello');
 
-    // redirect with named route
-    return redirect()->route('hello');
-});
+// // example of named route
+// Route::get('/hello', function(){
+//     return 'Hello';
+// })->name('hello');
 
-// dynamic url : example of route(url) with parameter
-Route::get('/greet/{name}', function ($name){
-    return 'Hello ' . $name . '!';
-});
+// // example tp redirect a route to another route
+// Route::get('/hallo', function(){
+//     // redirect with normal route url
+//     // return redirect('/hello');
+
+//     // redirect with named route
+//     return redirect()->route('hello');
+// });
+
+// // dynamic url : example of route(url) with parameter
+// Route::get('/greet/{name}', function ($name){
+//     return 'Hello ' . $name . '!';
+// });
 
 // this is route is triggered when requested url not found
 Route::fallback(function(){
