467dee55f5e578581aa4893c7f4a47166224c45e
ricky0995 <wijayaricky1295@gmail.com>
2023-08-01 16:47:02 +0700

    defining Task Factory and Databasse seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        
+        // this run the unverified function at UserFactory.php 
+        \App\Models\User::factory(2)->unverified()->create();
+        
+        \App\Models\Task::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
