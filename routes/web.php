65a29b86536d4b97def437ec8cba621cdbcf1218
ricky0995 <wijayaricky1295@gmail.com>
2023-08-07 16:45:44 +0700

    fetching data from database and remove the dummy class in web.php

--- routes/web.php
+++ routes/web.php
@@ -27,71 +27,21 @@ class Task
   }
 }
 
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
-
-
 Route::get('/', function(){
     return redirect()->route('tasks.index');
 });
 
 // To Use a Variable with anonymous function, need to type 'use' keyword
-Route::get('/tasks', function () use ($tasks) {
-    return view('index', [
-        'tasks' => $tasks
-        
-        // the value will be escaped by laravel, so html/script tag will be displayed as string
-        // 
-        // 'name' => 'Ricky',
-    ]);
-    // return view('welcome');
+Route::get('/tasks', function () {
+  return view('index', [
+    'tasks' => \App\Models\Task::latest()->get(),
+  ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-
-    // colect function is from laravel, it is a collection object with some function
-    $task = collect($tasks)->firstWhere('id', $id);
-    if(!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-    return view('show', ['task' => $task]);
-    // return 'One single task';
+Route::get('/tasks/{id}', function ($id) {
+  return view('show',[
+    'task' => \App\Models\Task::findOrFail($id),
+  ]);
 })->name('tasks.show');
 
 
