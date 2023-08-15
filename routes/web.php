7be9a2791dc6e3434aa68f218381d8d8ef3c4ae5
ricky0995 <wijayaricky1295@gmail.com>
2023-08-15 16:18:07 +0700

    add html form and post route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,11 +39,20 @@ Route::get('/tasks', function () {
   ]);
 })->name('tasks.index');
 
+
+Route::view('/tasks/create', 'create');
+
+
 Route::get('/tasks/{id}', function ($id) {
   return view('show',[
     'task' => \App\Models\Task::findOrFail($id),
   ]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request){
+  dd($request->all());
+})->name('tasks.store');
+
 
 
 // // example of named route
