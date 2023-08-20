33cc28dc61f76e1a7913068f70e81aa0575901a1
ricky0995 <wijayaricky1295@gmail.com>
2023-08-20 17:33:26 +0700

    refactor the post and put mehod and add the a TaskRequest.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    // in a model we need to define which column in db is permitted to modify, the column not specified below will not be able to modify
+    protected $fillable = ['title', 'description', 'long_description'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Models\Task;
+use App\Http\Requests\TaskRequest;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,49 +31,33 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create');
 
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function (Task $task) {
   return view('show',[
-    'task' => Task::findOrFail($id),
+    // 'task' => Task::findOrFail($id),
+    'task' => $task,
   ]);
 })->name('tasks.show');
 
 
-Route::get('/tasks/{id}/edit', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
   return view('edit',[
-    'task' => Task::findOrFail($id),
+    // 'task' => Task::findOrFail($id),
+    'task' => $task,
   ]);
 })->name('tasks.edit');
 
-Route::post('/tasks', function (Request $request){
-  // dd($request->all());
-  $data = $request->validate([
-    'title'             => 'required|max:255',
-    'description'       => 'required',
-    'long_description'  => 'required',
-  ]);
-  $task = new Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-
-  $task->save();
-  return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task created successfully');
+Route::post('/tasks', function (TaskRequest $request){
+  // a better way to store data than above codes
+  // the $request->validated() is a laravel function from $request object which validate input based on rules we set in TaskRequest.php located at app\http\request\
+  $task = Task::create($request->validated());
+  return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function ($id, Request $request){
-  // dd($request->all());
-  $data = $request->validate([
-    'title'             => 'required|max:255',
-    'description'       => 'required',
-    'long_description'  => 'required',
-  ]);
-  $task = Task::findOrFail($id);
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-
-  $task->save();
-  return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task updated successfully');
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
+  // dd($task);
+  // $task is model then get the task id from the url, therefore model update the data with the selected id
+  $task->update($request->validated());
+  return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully');
 })->name('tasks.update');
 
 // // example of named route
