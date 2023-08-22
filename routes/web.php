11c6f2c81728ce2df9459083ba473b995df1f3a2
ricky0995 <wijayaricky1295@gmail.com>
2023-08-22 15:47:06 +0700

    add delete functionality

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,13 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
   return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task){
+  $task->delete();
+  return redirect()->route('tasks.index')->with('success', 'Task Deleted Successfully');
+})->name('tasks.destroy');
+
+
+
 // // example of named route
 // Route::get('/hello', function(){
 //     return 'Hello';
